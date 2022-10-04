391284de5bddaca26d2b63d0f258b88db639beda
Paul Netia <netiapaul23@gmail.com>
2022-10-04 10:51:36 +0300

    - starter project created - Native element material added

---
+++ components/dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppBar } from "@react-native-material/core";
+
+function Dashboard() {
+  return (
+    // <View>
+    <>
+      <AppBar title="Screen title" />
+      <Text> Headed to greatness with Kinjoz</Text>
+    </>
+
+    // </View>
+  );
+}
+
+export default Dashboard;
