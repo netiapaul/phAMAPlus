5caf6af7bc945ef05c978ab7ef041e4e9fbdbcb5
Paul Netia <netiapaul23@gmail.com>
2022-10-06 19:57:13 +0300

    - Created common colors

--- components/landing.js
+++ components/landing.js
@@ -1,88 +1,74 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Colors from "../constants/colors";
 import {
   NativeBaseProvider,
-  Container,
   Heading,
   Center,
-  Flex,
   Pressable,
   Box,
+  Text,
+  Image,
 } from "native-base";
 function Landing() {
   return (
     <NativeBaseProvider>
       <View style={styles.container}>
         <View style={styles.box1}>
-          <Text>Get Started</Text>
+          <Center flex={1}>
+            <Image
+              source={{
+                uri: "https://wallpaperaccess.com/full/317501.jpg",
+              }}
+              alt="Alternate Text"
+              size="xl"
+            />
+          </Center>
         </View>
         <View style={styles.box2}>
-          <Heading>Find a way to manage</Heading>
-          <Heading>your Transactions</Heading>
-          <Text color="emerald.500"> Best Solution to save your</Text>
-          <Text color="emerald.500"> loyalty points & transactions</Text>
+          <Box p="5">
+            <Heading size="2xl">Find a way to manage</Heading>
+            <Heading size="2xl">your Transactions</Heading>
+            <Text fontSize="md">Best Solution to save your</Text>
+            <Text fontSize="md">loyalty points & transactions</Text>
+          </Box>
         </View>
         <View style={styles.box3}>
-          <Text>Get Started</Text>
+          {/* bg="indigo.700" */}
+          <Center flex={1}>
+            <Pressable
+              onPress={() => console.log("MegaJackpot Winner with Kinjoz")}
+            >
+              {({ isHovered, isFocused, isPressed }) => {
+                return (
+                  <Box
+                    borderColor="none"
+                    bg={
+                      isPressed
+                        ? Colors.phAMACoreColor1
+                        : Colors.phAMACoreColor2
+                    }
+                    minW="75%"
+                    rounded={"md"}
+                    p="5"
+                    style={{
+                      transform: [
+                        {
+                          scale: isPressed ? 1 : 1,
+                        },
+                      ],
+                    }}
+                  >
+                    <Center>
+                      <Text style={{ color: "white" }}>Get Started</Text>
+                    </Center>
+                  </Box>
+                );
+              }}
+            </Pressable>
+          </Center>
         </View>
       </View>
-      {/* <Flex direction="column">
-            <Center flex={3} minW="100%" bg="indigo.300">
-              <Heading>
-                A component library for the
-                <Text color="emerald.500"> React Ecosystem</Text>
-              </Heading>
-            </Center>
-            <Box flex={1} minW="100%" bg="indigo.500">
-              <Heading>Find a way to manage</Heading>
-              <Heading>your Transactions</Heading>
-              <Text color="emerald.500"> Best Solution to save your</Text>
-              <Text color="emerald.500"> loyalty points & transactions</Text>
-            </Box>
-            <Center flex={1} bg="indigo.700">
-              <Pressable
-                onPress={() => console.log("MegaJackpot Winner with Kinjoz")}
-              >
-                {({ isHovered, isFocused, isPressed }) => {
-                  return (
-                    <Box
-                      borderColor="none"
-                      bg={
-                        isPressed
-                          ? Colors.phAMACoreColor1
-                          : Colors.phAMACoreColor2
-                      }
-                      minW="75%"
-                      rounded={"md"}
-                      p="5"
-                      style={{
-                        transform: [
-                          {
-                            scale: isPressed ? 1 : 1,
-                          },
-                        ],
-                      }}
-                    >
-                      <Center>
-                        <Text style={{ color: "white" }}>Get Started</Text>
-                      </Center>
-                    </Box>
-                  );
-                }}
-              </Pressable>
-              <Button
-                minW="75%"
-                size={"lg"}
-                rounded={"none"}
-                p="5"
-                onPress={() => console.log("MegaJackpot Winner with Kinjoz")}
-                bg={Colors.phAMACoreColor2}
-              >
-                Click Me
-              </Button>
-            </Center>
-          </Flex> */}
     </NativeBaseProvider>
   );
 }
@@ -90,20 +76,18 @@ function Landing() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#C58C4F",
-    // alignItems: "center",
+    backgroundColor: Colors.phAMACoreColor1,
   },
   box1: {
     flex: 3,
-    backgroundColor: "red",
+    backgroundColor: Colors.danger,
   },
   box2: {
     flex: 1,
-    backgroundColor: "greeb",
+    justifyContent: "center",
   },
   box3: {
     flex: 1,
-    backgroundColor: "blue",
   },
   //   image: {
   //     maxWidth: 350,
--- constants/colors.js
+++ constants/colors.js
@@ -1,10 +1,7 @@
 export default {
-  primary: "#202c41",
-  border: "#c6c6c6",
-  white: "#fff",
-  gray: "#9ca5ab",
-  background: "#eef0f3",
-  error: "#b55464",
+  success: "#2eb85c",
+  warning: "#f9b115",
+  danger: "#e55353",
   phAMACoreColor1: "#c58c4f",
   phAMACoreColor2: "#5d3915",
 };
