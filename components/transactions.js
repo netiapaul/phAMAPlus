345df58089ce775a53f2eade207fc682f1d5cf24
Paul Netia <netiapaul23@gmail.com>
2022-12-18 15:54:01 +0300

    - No transactions notice

--- components/transactions.js
+++ components/transactions.js
@@ -30,8 +30,9 @@ function Transactions({ navigation }) {
     let token = await SecureStore.getItemAsync("token");
     let memberno = await SecureStore.getItemAsync("memberno");
     if (token || memberno) {
+      //  PP000008 PPL000031
       return fetch(
-        `http://102.37.102.247:5016/CustomerPoints/GetCustomerTransactions?memberNo=${"PP000008"}`,
+        `http://102.37.102.247:5016/CustomerPoints/GetCustomerTransactions?memberNo=${"PPL000031"}`,
         {
           method: "GET", // GET, POST, PUT, DELETE, etc.
           headers: {
@@ -94,20 +95,20 @@ function Transactions({ navigation }) {
         >
           <Heading size="md"> Transaction</Heading>
         </HStack> */}
-        {isLoading ? (
-          <Center flex={1}>
-            <ActivityIndicator size="large" color="#0000ff" />
-          </Center>
-        ) : (
-          <ScrollView
-            // bg="indigo.200"
-            px={3}
-            refreshControl={
-              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }
-          >
-            {transactions ? (
-              transactions.map((transaction, index) => (
+        {
+          isLoading ? (
+            <Center flex={1}>
+              <ActivityIndicator size="large" color="#0000ff" />
+            </Center>
+          ) : transactions.length > 0 ? (
+            <ScrollView
+              // bg="indigo.200"
+              px={3}
+              refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+              }
+            >
+              {transactions.map((transaction, index) => (
                 <Link
                   onPress={() =>
                     navigation.navigate("transactionDetails", {
@@ -208,21 +209,167 @@ function Transactions({ navigation }) {
                     </HStack>
                   </Box>
                 </Link>
-              ))
-            ) : (
+              ))}
+            </ScrollView>
+          ) : (
+            <Center flex={1}>
               <Text
                 color="coolGray.600"
-                _dark={{
-                  color: "warmGray.200",
-                }}
-                fontWeight="500"
-                fontSize="xs"
+                // _dark={{
+                //   color: "warmGray.200",
+                // }}
+                fontWeight="700"
+                fontSize="2xl"
               >
                 No Transaction History
               </Text>
-            )}
-          </ScrollView>
-        )}
+              {/* <ScrollView
+                bg="indigo.100"
+                // px={3}
+                refreshControl={
+                  <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                  />
+                }
+              >
+                <Text
+                  color="coolGray.600"
+                  // _dark={{
+                  //   color: "warmGray.200",
+                  // }}
+                  fontWeight="500"
+                  // fontSize="xs"
+                >
+                  No Transaction History
+                </Text>
+              </ScrollView> */}
+            </Center>
+          )
+          // <ScrollView
+          //   // bg="indigo.200"
+          //   px={3}
+          //   refreshControl={
+          //     <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          //   }
+          // >
+          //   {transactions.length <= 0 ? (
+          //     transactions.map((transaction, index) => (
+          //       <Link
+          //         onPress={() =>
+          //           navigation.navigate("transactionDetails", {
+          //             docNum: transaction.DOCNUM,
+          //             salesBCODE: transaction.SALESBCODE,
+          //             branch: transaction.SALESBRANCH,
+          //             gain: transaction.MEMPOINTSBUY,
+          //             redeemed: transaction.MEMPOINTSREDEEM,
+          //             transdate: transaction.SALEDATE,
+          //           })
+          //         }
+          //         key={index}
+          //       >
+          //         <Box
+          //           width="100%"
+          //           p="3"
+          //           my={1}
+          //           rounded="md"
+          //           bg="coolGray.100"
+          //           borderWidth={1}
+          //           // shadow={2}
+          //         >
+          //           <HStack justifyContent="space-between" alignItems="center">
+          //             <VStack>
+          //               <Text
+          //                 color={"muted.800"}
+          //                 fontWeight="600"
+          //                 fontSize={"sm"}
+          //               >
+          //                 {transaction.DOCNUM}
+          //               </Text>
+          //               <HStack>
+          //                 <Text
+          //                   color={"muted.800"}
+          //                   fontWeight="400"
+
+          //                   // style={styles.myText}
+          //                   // fontSize={'8'}
+          //                 >
+          //                   {new Date(transaction.SALEDATE).toDateString()}
+          //                 </Text>
+          //                 <Center>
+          //                   <Divider orientation="vertical" h={3} mx={1} />
+          //                 </Center>
+
+          //                 <Text
+          //                   color={"muted.800"}
+          //                   fontWeight="400"
+
+          //                   // style={styles.myText}
+          //                   // fontSize={'8'}
+          //                 >
+          //                   {transaction.SALESBRANCH}
+          //                 </Text>
+          //               </HStack>
+          //               <Text
+          //                 color={"muted.800"}
+          //                 fontWeight="600"
+          //                 fontSize={"sm"}
+          //               >
+          //                 Kshs.
+          //                 {transaction.Itmtotalinc.toFixed(2)
+          //                   .toString()
+          //                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          //               </Text>
+          //               <HStack space={3}>
+          //                 <Text
+          //                   color="success.600"
+          //                   fontWeight="400"
+          //                   // style={styles.myText}
+          //                 >
+          //                   Earned:{" "}
+          //                   {transaction.MEMPOINTSBUY.toString().replace(
+          //                     /\B(?=(\d{3})+(?!\d))/g,
+          //                     ","
+          //                   )}
+          //                 </Text>
+
+          //                 <Text
+          //                   color="danger.600"
+          //                   fontWeight="400"
+          //                   // style={styles.myText}
+          //                 >
+          //                   Redeemed:{" "}
+          //                   {transaction.MEMPOINTSREDEEM.toString().replace(
+          //                     /\B(?=(\d{3})+(?!\d))/g,
+          //                     ","
+          //                   )}
+          //                 </Text>
+          //               </HStack>
+          //             </VStack>
+          //             <Image
+          //               source={require("../assets/right.png")}
+          //               alt="company logo"
+          //               // style={styles.companyLogo}
+          //               size="2xs"
+          //             />
+          //           </HStack>
+          //         </Box>
+          //       </Link>
+          //     ))
+          //   ) : (
+          //     <Text
+          //       color="coolGray.600"
+          //       _dark={{
+          //         color: "warmGray.200",
+          //       }}
+          //       fontWeight="500"
+          //       // fontSize="xs"
+          //     >
+          //       No Transaction History
+          //     </Text>
+          //   )}
+          // </ScrollView>
+        }
       </View>
     </NativeBaseProvider>
   );
